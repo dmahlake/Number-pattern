01855d4c59dc6f8a8580700798cd30fd3c0e303c
Dipuo Mahlake <30497517+dmahlake@users.noreply.github.com>
2019-11-13 02:23:18 -0800

    Add files via upload

---
+++ practice2.java
@@ -0,0 +1,27 @@
+class practice2{
+    public static void main(String[] args) {
+       
+        
+
+        for (int i = 3; i <= 12; i++)
+        {
+            for (int k = i; k <= 12; k++)
+                {
+                    System.out.print(k + ":");
+                }
+                System.out.println("");
+        }
+
+        System.out.println("\n");
+        for (int j = 12; j>= 3; j--)
+        {
+            for (int c = j; c >= 3; c--)
+            {
+               System.out.print(c + ":"); 
+            }
+            System.out.println("");
+        }
+
+       
+    }
+}
